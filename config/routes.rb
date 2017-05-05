86a28e67fed967868b01ba7ae30c74cf44218a1d
Alan Ning <askldjd+github@gmail.com>
2017-05-05 16:13:48 -0400

    Remove root redirect (#17)
    Removed root redirect because when APP_URL_PREFIX is not defined, it leads to an infinite redirect loop.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  root :to => redirect("#{Rails.application.config.app_url_prefix}")
-
   scope "#{Rails.application.config.app_url_prefix}" do
     get "/" => 'monitor#index'
     get "/sample" => 'sample#index'
