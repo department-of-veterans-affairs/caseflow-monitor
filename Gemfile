0e9757d120432472c4eb60df63ed3f18121e5704
Alan Ning <askldjd+github@gmail.com>
2017-08-08 15:29:58 -0400

    Added VACOLS deep monitoring  (#31)
    Merging this in to test out Grafana charts. It is difficult to tell how the charts look without moving this to UAT.

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'jbuilder', '~> 2.5'
 # For sending metrics to prometheus
 gem 'prometheus-client'
 
+gem 'tzinfo-data'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -55,5 +56,3 @@ group :development do
   gem 'listen', '~> 3.0.5'
 end
 
-# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/services/vacols_service.rb
+++ app/services/vacols_service.rb
@@ -9,7 +9,7 @@ class VacolsService < MonitorService
     @name = @@service_name
     @service = "VACOLS"
     @env = ENV['VACOLS_HOST']
-    @api = "VACOLS.BRIEFF"
+    @api = "ASH"
     super
   end
 
@@ -38,16 +38,95 @@ class VacolsService < MonitorService
     end
 
     begin
-      filenum = Rails.application.secrets.target_file_num.split(",").first.strip
-      array = @connection.exec_query("SELECT * FROM VACOLS.BRIEFF where BFKEY='#{filenum}'")
+
+      latency_gauge = Prometheus::Client.registry.get(:vacols_performance)
+
+      query = <<-SQL
+        select user_id from DBA_USERS where username = 'DSUSER'
+      SQL
+      user_id_result = @connection.exec_query(query)
+      dsuser_id = user_id_result[0]['user_id']
+
+      # In the Oracle performance metric, we focus on DB Time, where
+      # DB Time = DB CPU + non_idle_wait_time
+      #
+      # DB Time captures the total amount of time the DB is consuming, and it
+      # breaks down to CPU time, and non-idle wait such as latch and lock.
+      # See also: http://blog.orapub.com/20140805/what-is-oracle-db-time-db-cpu-wall-time-and-non-idle-wait-time.html
+
+
+      # A continuous increment of total non-idle wait time by class. The wait class
+      # provides a breakdown of where they are occurring.
+      # 
+      query = <<-SQL
+        select e.wait_class "wait_event", 
+          sum(h.wait_time + h.time_waited) "total_wait_time"
+        from v$active_session_history h, v$event_name e
+        where h.event_id = e.event_id
+          and e.wait_class <> 'idle'
+        group by e.wait_class
+        order by 2 desc
+      SQL
+      wait_time_by_class = @connection.exec_query(query)
+      wait_time_by_class.each do |wtc|
+        latency_gauge.set({
+          source: 'ash',
+          name: wtc['wait_event']
+        }, wtc['total_wait_time'])
+      end
+    
+      # Overall system time that includes DB Time, DB CPU and various metrics
+      query = <<-SQL
+        select stat_name, value "time"
+        from v$sys_time_model
+      SQL
+      sys_time_model = @connection.exec_query(query)
+      sys_time_model.each do |stm|
+        latency_gauge.set({
+          source: 'sys_time_model',
+          name: stm['stat_name']
+        }, stm['time'])
+      end
+
+
+      # Summing DB Time from ASH table
+      query = <<-SQL
+        select count(*) DBTime
+        from v$active_session_history
+        where sample_time > sysdate - 1
+          and session_type <> 'BACKGROUND'
+        order by count(*) desc
+      SQL
+      sum_all_db_time_24hrs = @connection.exec_query(query)
+      latency_gauge.set({
+        source: 'ash',
+        name: 'sum_all_db_time_24hrs'
+      }, sum_all_db_time_24hrs[0]['dbtime'])
+
+      # Summing Caseflow DB Time from ASH table
+      caseflow_db_time_24hrs = @connection.exec_query(<<-EQL)
+        select count(*) DBTime
+        from v$active_session_history
+        where sample_time > sysdate - 1
+          and session_type <> 'BACKGROUND'
+          and v$active_session_history.user_id = #{dsuser_id}
+        order by count(*) desc
+      EQL
+      latency_gauge.set({
+        source: 'ash',
+        name: 'caseflow_db_time_24hrs'
+      }, caseflow_db_time_24hrs[0]['dbtime'])
+
     rescue => e
+      Rails.logger.warn(e.message)
+
       # If this is a connectivity issue, reset the connection pointer and
       # force the connection to be re-established in the next query.
       if e.original_exception.is_a?(OCIError) &&
          LOST_CONNECTION_ERROR_CODES.include?(e.original_exception.code)
-        puts "VACOLS connection dropped, reconnecting on next query"
+        Rails.logger.warn("VACOLS connection dropped, reconnecting on next query")
         @connection = nil
-      end
+      end      
 
       # Propagate the exception up the stack to fail this query. This way, the
       # failure will be recorded in Prometheus / Grafana.
