4bcf21d5f761af0328a51a107e0c6f17e3009f20
Alan Ning <askldjd+github@gmail.com>
2017-08-08 09:47:43 -0400

    saving work

--- build-container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Build the appeals development image. This should be run at least
-# once prior to running docker-compose.
-#
-docker build \
-    --build-arg username=$USER \
-    --build-arg usergroup=`id -g -n $USER` \
-    -t appeals-caseflow-monitor-img .
