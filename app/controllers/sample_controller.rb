d69a9a03f1bff57cec8db8ba508f31d765a54e57
Nemo Djuric <nemo.djuric@gmail.com>
2017-11-08 12:46:47 -0500

    Added outage info api.

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -1,17 +1,38 @@
 class SampleController < ApplicationController
-  def index
 
-    results = {}
+  attr_accessor :results
+
+  def index
+    get_data
+    respond_to do |format|
+      format.json { render(json: @results.as_json ) }
+    end
+  end
+
+  def services
+    get_data
+      services_results = @results.values.each_with_object([]) do |element, boolean_result|
+        if element["up_rate_5"].to_i < 50
+          boolean_result << { element[:name] => :false }
+        else
+          boolean_result << { element[:name] => :true }
+        end
+      end
+    respond_to do |format|
+      format.json { render(json: services_results.as_json ) }
+    end
+  end
+
+  def get_data
+    @results = {}
 
     Rails.application.config.monitor_services.each do |service|
       datapoint = Rails.cache.read(service.service_name)
 
       if datapoint != nil
-        results[service.service_name.to_sym] = datapoint
+        @results[service.service_name.to_sym] = datapoint
       end
     end
-    respond_to do |format|
-      format.json { render(json: results.as_json ) }
-    end
+    @results
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   scope "#{Rails.application.config.app_url_prefix}" do
     get "/" => 'monitor#index'
     get "/sample" => 'sample#index'
+    get "/services" => 'sample#services'
   end
 
 end
