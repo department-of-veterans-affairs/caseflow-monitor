d956a923a3ab292c697c8c9afe310f914f6e6851
Nemo Djuric <nemo.djuric@gmail.com>
2017-11-13 09:12:28 -0500

    Changed naming and instance variable.

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -1,38 +1,34 @@
 class SampleController < ApplicationController
 
-  attr_accessor :results
-
   def index
-    get_data
     respond_to do |format|
-      format.json { render(json: @results.as_json ) }
+      format.json { render(json: fetch_uptime_data.as_json ) }
     end
   end
 
-  def services
-    get_data
-      services_results = @results.values.each_with_object([]) do |element, boolean_result|
-        if element[:up_rate_5].to_i < 50
-          boolean_result << { element[:name] => :false }
+  def availability_report
+      availability_results = fetch_uptime_data.values.each_with_object([]) do |service, uptime_report|
+        if service[:up_rate_5].to_i < 50
+          uptime_report << { service[:name] => :false }
         else
-          boolean_result << { element[:name] => :true }
+          uptime_report << { service[:name] => :true }
         end
       end
     respond_to do |format|
-      format.json { render(json: services_results.as_json ) }
+      format.json { render(json: availability_results.as_json ) }
     end
   end
 
-  def get_data
-    @results = {}
+  def fetch_uptime_data
+    results = {}
 
     Rails.application.config.monitor_services.each do |service|
       datapoint = Rails.cache.read(service.service_name)
 
       if datapoint != nil
-        @results[service.service_name.to_sym] = datapoint
+        results[service.service_name.to_sym] = datapoint
       end
     end
-    @results
+    results
   end
 end
