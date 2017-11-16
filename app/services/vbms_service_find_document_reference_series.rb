fa16ad2d9c986ac7df9c845453ed67549cff91b5
Alex Prokop <alex.prokop@gmail.com>
2017-11-16 09:44:59 -0500

    tweak

--- app/services/vbms_service_find_document_reference_series.rb
+++ app/services/vbms_service_find_document_reference_series.rb
@@ -12,7 +12,7 @@ class VBMSServiceFindDocumentReferenceSeries < MonitorService
 
     @client = VBMS::Client.from_env_vars(
       env_name: ENV["CONNECT_VBMS_ENV"],
-      use_forward_proxy: ENV["CONNECT_VBMS_BASE_PROXY_URL"].present?
+      use_forward_proxy: ENV["CONNECT_VBMS_PROXY_BASE_URL"].present?
     )
     super
   end
